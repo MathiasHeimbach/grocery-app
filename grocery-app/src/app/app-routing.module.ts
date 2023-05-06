952451963ff5c273782aae49f6106db60a423eab
MathiasHeimbach <60534997+MathiasHeimbach@users.noreply.github.com>
2023-05-06 11:56:52 +0200

    Lesson2 (#2)
    * add contact and about pages
    
    * add error page

--- grocery-app/src/app/app-routing.module.ts
+++ grocery-app/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home', component: HomePageComponent
+  },
+  { path: 'products/:catId', component: ProductPageComponent },
+  { path: 'products/detail/:id', component: ProductDetailPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contact', component: ContactPageComponent }
+  ,
+  { path: '**', component: ErrorComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
